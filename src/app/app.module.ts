af791bbbb6b304e165b557aabae55135316076e9
Diego Torres <dtorres@number8.com>
2020-10-17 11:12:33 -0500

    Main components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommitListComponent } from './commit-list/commit-list.component';
+import { CommitItemComponent } from './commit-list/commit-item/commit-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommitListComponent,
+    CommitItemComponent
   ],
   imports: [
     BrowserModule
